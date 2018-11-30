238b54c0eebeb7fbc1d6bfb08e95d2caa35b504f
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-11-30 13:55:52 +0100

    finished two solutions

--- product.py
+++ product.py
@@ -1,4 +1,21 @@
 def product(integers):
-  total = 0
-  for digit in integers:
+    total = 1
+    for digit in integers:
+        total *= digit
+    return total
+
+
+print(product([4, 5, 5]))
+
+
+def productWhile(integers):
+    total = 1
+    length = len(integers)
+    digits = integers[:]
+    while length > 0:
+        total *= digits[length - 1]
+        length -= 1
+    return total
+
+print(productWhile([4, 5, 5, 6, 7]))
     
